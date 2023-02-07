fb8b40654e5c9b6b9def827e9d130df67f440ad9
Daniela <ddiazgraziani@gmail.com>
2023-02-07 16:27:48 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [text, setText] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const config = new Configuration({
+    apiKey: "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",
+  });
+
+  const api = new OpenAIApi(config);
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    api
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: generatePrompt(text),
+        temperature: 0.6,
+        max_tokens: 100,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          setLoading(false);
+          if (res.data.choices[0] && res.data.choices[0].text) {
+            setResponse(res.data.choices[0].text);
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error, "ocurrio un error");
+      });
+  };
+
+  const generatePrompt = (text: string) => {
+    return `Summarize this ${text} and break them into seperate lines`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <h1>
+            Texto <span>Resumido</span>
+          </h1>
+          <h2> Resume tu texto y hazlo más corto.</h2>
+        </div>
+      </div>
+      <div>
+        <div>
+          <form>
+            <h3>Ingresa el Texto</h3>
+            <textarea
+              className="form-control"
+              rows={14}
+              cols={80}
+              placeholder="Ingresa el texto a resumir"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </form>
+        </div>
+        <div>
+          <button type="button" onClick={handleSubmit}>
+            {loading ? "Cargando, espere por favor ..." : "Resumir"}
+          </button>
+        </div>
+        <div>
+          <h3>Texto Resumido</h3>
+          <textarea
+            className="form-control"
+            placeholder="Texto Resumido"
+            cols={80}
+            rows={14}
+            value={response}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
